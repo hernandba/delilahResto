874e1f44519ae84f886e05e2d4c665eb79dfd4d3
Hernan Belalcazar <hernandba@gmail.com>
2021-06-09 12:27:53 -0500

    Add id_order to results, change order results({order_detail,products_detail})

--- src/database/orders/getOrderDetailsById.js
+++ src/database/orders/getOrderDetailsById.js
@@ -12,14 +12,14 @@ async function getOrderDetailsById(id_order){
         )
 
         const order_detail = await sequelize.query(
-            "SELECT SUM(products.price*products_per_order.product_quantity) total, situation.situation, payment.method, users.fullname, users.username, users.email, users.phone, users.address FROM orders JOIN situation ON orders.id_situation = situation.id_situation JOIN payment ON orders.id_payment = payment.id_payment JOIN users ON orders.id_user = users.id_user JOIN products_per_order ON orders.id_order = products_per_order.id_order JOIN products ON products_per_order.id_product = products.id_product WHERE orders.id_order = :id_order",
+            "SELECT orders.id_order, SUM(products.price*products_per_order.product_quantity) total, situation.situation, payment.method, users.fullname, users.username, users.email, users.phone, users.address FROM orders JOIN situation ON orders.id_situation = situation.id_situation JOIN payment ON orders.id_payment = payment.id_payment JOIN users ON orders.id_user = users.id_user JOIN products_per_order ON orders.id_order = products_per_order.id_order JOIN products ON products_per_order.id_product = products.id_product WHERE orders.id_order = :id_order",
             {   
                 replacements: {id_order: id_order},
                 type: sequelize.QueryTypes.SELECT
             }
         )
 
-        const result = {products_detail: products_detail, order_detail:order_detail}
+        const result = {order_detail:order_detail, products_detail: products_detail}
        
         //Retorna Arreglo con todos los favs    
         return result;
