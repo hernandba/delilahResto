9fcedb5d09c6c8618c8bd3932ca058160d8a591d
Hernan Belalcazar <hernandba@gmail.com>
2021-06-09 16:05:26 -0500

    Commented if id_user undefined (unnecesary)

--- src/validations/users/validateUserId.js
+++ src/validations/users/validateUserId.js
@@ -3,12 +3,12 @@ const searchUser = require('../../database/users/searchUser');
 const validateUserId = (req, res, next) => {
     const {id_user} = req.params;
 
-    if(!id_user){
-        return res.status(400).send({
-            status: 'error',
-            message: 'Uncomplete Data'
-        })
-    }
+    // if(!id_user){
+    //     return res.status(400).send({
+    //         status: 'error',
+    //         message: 'Uncomplete Data'
+    //     })
+    // }
 
     searchUser({username: "", email: "", id_user: id_user}).then(result => {
         const userExists = result;
