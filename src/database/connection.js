328d9db95c1bc887d0c26adc9d026a2ff47cc61c
Hernan Belalcazar <hernandba@gmail.com>
2021-06-15 21:16:29 -0500

    Added Connection to BD parameters to .env

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
+const { DB_HOST, DB_NAME, S_USER, S_PASSWORD, DB_PORT } = process.env
+
 const Sequelize = require('sequelize');
-const path = 'mysql://root@localhost:3306/delilahresto';
-const sequelize = new Sequelize(path);
+// const path = 'mysql://root@localhost:3306/delilahresto';
+// const sequelize = new Sequelize(path);
+const sequelize = new Sequelize(DB_NAME, S_USER, S_PASSWORD, 
+    {
+        host: DB_HOST,
+        dialect: 'mysql',
+        port: DB_PORT,
+        dialectOptions: {
+            multipleStatements: true
+        }
+    }
+)
 
 sequelize.authenticate()
-    .then(() => console.log('Connected to BD delilahresto...'))
+    .then(() => console.log('Connected to BD',DB_NAME))
     .catch(error => console.error(error));
 
 module.exports = sequelize;
